a7a0a108b7db7176ec9c5c2bab751c4bb947f435
Luca Tironi <luca.tironi@gmail.com>
2015-04-07 17:56:36 +0200

    Added Github login/logout and protect the User::Profile controller behind authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root 'users/profile#show'
+
+  get '/login'  => 'users/sessions#new',     as: :login
+  get '/logout' => 'users/sessions#destroy', as: :logout
+  get '/auth/:provider/callback' => 'users/sessions#create'
 end
